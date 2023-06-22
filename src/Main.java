327961cb84c745767a091a6c92eb8dd2323c367c
okamodgi <nadakupnada@gmail.com>
2023-06-22 17:49:48 +0300

    laboratornaya19

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Calculator calc = Calculator.a;
+        calc.setCalculator(5, 43);
+        calc.calculateFunction();
+    }
+}
+enum Calculator{
+    a;
+    private int x, y;
+    public void setCalculator(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    public void calculateFunction(){
+        double rez = 2*x+3/y;
+        System.out.println(rez);
     }
 }
